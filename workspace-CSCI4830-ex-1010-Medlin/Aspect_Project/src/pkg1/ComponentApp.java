157a837ff583c8f66aa9ba983e0d463ea41f0476
Jax-Medlin <54479037+Jax-Medlin@users.noreply.github.com>
2023-10-10 08:39:45 -0500

    Add files via upload

---
+++ workspace-CSCI4830-ex-1010-Medlin/Aspect_Project/src/pkg1/ComponentApp.java
@@ -0,0 +1,14 @@
+package pkg1;
+
+public class ComponentApp {
+	private String name;
+
+	public ComponentApp(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
